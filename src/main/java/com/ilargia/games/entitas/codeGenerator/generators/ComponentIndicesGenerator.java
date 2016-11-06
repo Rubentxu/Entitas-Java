d258f5c234e51522f2fa4f07ca552f849e124fb8
Rubentxu <rubentxu74@gmail.com>
2016-11-06 10:18:25 +0100

    component generator

--- src/main/java/com/ilargia/games/entitas/codeGenerator/generators/ComponentIndicesGenerator.java
+++ src/main/java/com/ilargia/games/entitas/codeGenerator/generators/ComponentIndicesGenerator.java
@@ -1,11 +1,13 @@
 package com.ilargia.games.entitas.codeGenerator.generators;
 
 
+import com.ilargia.games.entitas.Pool;
 import com.ilargia.games.entitas.codeGenerator.CodeGenerator;
 import com.ilargia.games.entitas.codeGenerator.interfaces.IComponentCodeGenerator;
 import com.ilargia.games.entitas.codeGenerator.interfaces.IPoolCodeGenerator;
 import com.ilargia.games.entitas.codeGenerator.intermediate.CodeGenFile;
 import com.ilargia.games.entitas.codeGenerator.intermediate.ComponentInfo;
+import org.jboss.forge.roaster.Roaster;
 import org.jboss.forge.roaster.model.source.JavaClassSource;
 
 import java.util.Arrays;
@@ -32,81 +34,97 @@ public class ComponentIndicesGenerator implements IComponentCodeGenerator, IPool
     }
 
     private JavaClassSource generateIndicesLookup(String lookupTag, ComponentInfo[] componentInfos) {
-        return addClassHeader(lookupTag)
-                + addIndices(componentInfos)
-                + addComponentNames(componentInfos)
-                + addComponentTypes(componentInfos)
-                + addCloseClass();
+        JavaClassSource javaClass = Roaster.parse(JavaClassSource.class, String.format("public static class {0} {}", lookupTag));
+        addIndices(componentInfos, javaClass);
+        addComponentNames(componentInfos, javaClass);
+        addComponentTypes(componentInfos, javaClass);
+        return javaClass;
+
     }
 
-    public static String addClassHeader(String lookupTag) {
-        return String.format("public static class {0} {{\n", lookupTag);
-    }
 
-    public static String addIndices(ComponentInfo[] componentInfos) {
-        String fieldFormat = "    public static final int {0} = {1};\n";
-        String totalFormat = "    public static final int TotalComponents = {0};";
-        String code = "";
-        for (int i = 0; i < componentInfos.length; i++) {
+    public JavaClassSource addIndices(ComponentInfo[] componentInfos, JavaClassSource javaClass) {
+        for (Integer i = 0; i < componentInfos.length; i++) {
             ComponentInfo info = componentInfos[i];
             if (info != null) {
-                code += String.format(fieldFormat, info.typeName, i);
+                javaClass.addField()
+                        .setName(capitalize(info.typeName))
+                        .setType("int")
+                        .setLiteralInitializer(i.toString())
+                        .setStatic(true)
+                        .setFinal(true)
+                        .setPublic();
             }
         }
+        javaClass.addField()
+                .setName("totalComponents")
+                .setType("int")
+                .setLiteralInitializer(Integer.toString(componentInfos.length))
+                .setStatic(true)
+                .setFinal(true)
+                .setPublic();
 
-        if (code != "") {
-            code = "\n" + code;
-        }
 
-        String totalComponents = String.format(totalFormat, componentInfos.length);
-        return code + "\n" + totalComponents;
+        return javaClass;
+
     }
 
-    public static String addComponentNames(ComponentInfo[] componentInfos) {
+    public void addComponentNames(ComponentInfo[] componentInfos, JavaClassSource javaClass) {
         String format = "        \"{1}\",\n";
-        String nullFormat = "        null,\n";
-        String code = "";
+        String code = " return new String[] {";
         for (int i = 0; i < componentInfos.length; i++) {
             ComponentInfo info = componentInfos[i];
             if (info != null) {
                 code += String.format(format, i, info.typeName);
-            } else {
-                code += nullFormat;
             }
         }
         if (code.endsWith(",\n")) {
-            code = code.replace("\n", "");
+            code = code.replace(",\n", " }");
         }
-
-        return String.format(String.join("\n",
-                "public static String[] componentNames = {{ {0} }};", code));
+        javaClass.addMethod()
+                .setName("componentNames")
+                .setReturnType("String[]")
+                .setStatic(true)
+                .setPublic()
+                .setBody(code);
 
     }
 
-    public static String addComponentTypes(ComponentInfo[] componentInfos) {
+    public void addComponentTypes(ComponentInfo[] componentInfos, JavaClassSource javaClass) {
         String format = "        typeof({1}),\n";
-        String nullFormat = "        null,\n";
-        String code = "";
+        String code = "return new Class[] {";
         for (int i = 0; i < componentInfos.length; i++) {
             ComponentInfo info = componentInfos[i];
             if (info != null) {
                 code += String.format(format, i, info.fullTypeName);
-            } else {
-                code += nullFormat;
             }
         }
         if (code.endsWith(",\n")) {
-            code = code.replace("\n", "");
+            code = code.replace("\n", " }");
         }
 
-        return String.format(String.join("\n",
-                "public static System.Type[] componentTypes = {{ {{ {0} }};", code));
-
+        javaClass.addMethod()
+                .setName("componentTypes")
+                .setReturnType("Class[]")
+                .setStatic(true)
+                .setPublic()
+                .setBody(code);
 
     }
 
-    static String addCloseClass() {
-        return "\n}\n";
+
+    private String capitalize(final String string) {
+        char[] chars = string.toLowerCase().toCharArray();
+        boolean found = false;
+        for (int i = 0; i < chars.length; i++) {
+            if (!found && Character.isLetter(chars[i])) {
+                chars[i] = Character.toUpperCase(chars[i]);
+                found = true;
+            } else if (Character.isWhitespace(chars[i]) || chars[i] == '.' || chars[i] == '\'') { // You can add other chars here
+                found = false;
+            }
+        }
+        return String.valueOf(chars);
     }
 
 }
