1f6b0bafab71ac501e6d9fa6e56eee9ec9428f56
RubentxuEJ <rubentxu74@gmail.com>
2017-05-30 17:24:01 +0200

    Testing en plugins codegen

--- codegenerator/src/test/groovy/ilargia/entitas/gradle/CodegenPluginSpec.groovy
+++ codegenerator/src/test/groovy/ilargia/entitas/gradle/CodegenPluginSpec.groovy
@@ -2,13 +2,27 @@ package ilargia.entitas.gradle
 
 import org.gradle.api.Project
 import org.gradle.api.Task
+import org.gradle.api.file.SourceDirectorySet
 import org.gradle.testfixtures.ProjectBuilder
 import spock.lang.Specification
 
 @groovy.transform.TypeChecked
 class CodegenPluginSpec extends Specification {
 
-    void 'Consultamos al proveedor ComponentDataProvider por los contextos extraidos de los componentes'() {
+    void 'Queremos comporbar que se genero correctamente el Plugin de generacion de codigo'() {
+        given:
+        Project project = ProjectBuilder.builder().build()
+
+        when:
+        project.getPlugins().apply 'ilargia.entitas.gradle.codegen.plugin'
+
+        then:
+        project.getTasks().getByPath("codegen") instanceof CodeGenerationTask
+
+
+    }
+
+    void 'Queremos comporbar que se genero correctamente la tarea de generacion de codigo'() {
         given:
         Project project = ProjectBuilder.builder().build()
 
@@ -21,16 +35,20 @@ class CodegenPluginSpec extends Specification {
 
     }
 
-    void 'Consultamos2 al proveedor ComponentDataProvider por los contextos extraidos de los componentes'() {
+
+    void 'Queremos comporbar que se genero correctamente la configuracion de generacion de codigo'() {
         given:
         Project project = ProjectBuilder.builder().build()
-
-        when:
         project.getPlugins().apply 'ilargia.entitas.gradle.codegen.plugin'
 
-        then:
-        project.getTasks().getByPath("codegen") instanceof CodeGenerationTask
+        when:
+        CodeGenerationPluginExtension ext = project.getExtensions().getByName("EntitasSetting") as CodeGenerationPluginExtension
 
+        then:
+        ext.getProperties().get("configCodeGen") == "Entitas.properties"
 
     }
+
+
+
 }
