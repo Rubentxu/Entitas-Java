428019b55765924d6c496f243b265cfbd416a221
RubentxuEJ <rubentxu74@gmail.com>
2017-06-09 15:07:39 +0200

    testin en ComponentLookupGenerator

--- codegenerator/plugins/src/main/java/ilargia/entitas/codeGeneration/plugins/generators/ComponentLookupGenerator.java
+++ codegenerator/plugins/src/main/java/ilargia/entitas/codeGeneration/plugins/generators/ComponentLookupGenerator.java
@@ -9,11 +9,11 @@ import ilargia.entitas.codeGeneration.plugins.dataProviders.components.Component
 import org.apache.commons.lang3.text.WordUtils;
 import org.jboss.forge.roaster.Roaster;
 import org.jboss.forge.roaster.model.source.JavaClassSource;
-import org.jboss.forge.roaster.model.source.JavaInterfaceSource;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
+import static ilargia.entitas.codeGeneration.plugins.dataProviders.components.providers.ComponentTypeDataProvider.getFullTypeName;
 import static ilargia.entitas.codeGeneration.plugins.dataProviders.components.providers.ComponentTypeDataProvider.getTypeName;
 import static ilargia.entitas.codeGeneration.plugins.dataProviders.components.providers.ContextsDataProvider.getContextNames;
 import static ilargia.entitas.codeGeneration.plugins.dataProviders.components.providers.ShouldGenerateMethodsDataProvider.shouldGenerateMethods;
@@ -22,18 +22,17 @@ import static ilargia.entitas.codeGeneration.plugins.generators.ComponentEntityG
 
 public class ComponentLookupGenerator implements ICodeGenerator<JavaClassSource>, IConfigurable {
     private TargetPackageConfig targetPackageConfig;
-    private Map<String, CodeGenFile<JavaClassSource>> lookups;
+
     private Map<String, List<ComponentData>> contextDatas;
 
     public ComponentLookupGenerator() {
         targetPackageConfig = new TargetPackageConfig();
-        lookups = new HashMap<>();
         contextDatas = new HashMap<>();
     }
 
     @Override
     public String getName() {
-        return "Component (Matcher API)";
+        return "Components Lookup";
     }
 
     @Override
@@ -63,99 +62,105 @@ public class ComponentLookupGenerator implements ICodeGenerator<JavaClassSource>
 
     @Override
     public List<CodeGenFile<JavaClassSource>> generate(List<CodeGeneratorData> data) {
-        List<ComponentData> finalDatas = data.stream()
+        data.stream()
                 .filter(d -> d instanceof ComponentData)
                 .map(d -> (ComponentData) d)
                 .filter(d -> shouldGenerateMethods(d))
-                .sorted((a,b)-> a.getSource().getName().compareTo(b.getSource().getName()))
+                .sorted((a, b) -> a.getSource().getName().compareTo(b.getSource().getName()))
+                .forEach(d -> storeContextData(d));
+
+        List<CodeGenFile<JavaClassSource>> result =  contextDatas.entrySet().stream()
+                .map((entry) -> {
+                    JavaClassSource codeGen = generateIndicesLookup(entry.getKey(), entry.getValue());
+                    return new CodeGenFile<JavaClassSource>(codeGen.getName(), codeGen, entry.getValue().get(0).getSubDir());
+                })
                 .collect(Collectors.toList());
 
-        finalDatas.stream().forEach(d -> storeContextData(d));
+        contextDatas.clear();
+        return result;
 
-        contextDatas.forEach((k, v)-> generateIndicesLookup(k, v));
-
-        lookups.values().forEach(f -> System.out.println(f.getFileContent().toString()));
-        return lookups.values().stream().collect(Collectors.toList());
     }
 
     private void storeContextData(ComponentData data) {
-        getContextNames(data).stream().forEach(contextName -> {
-            if (!contextDatas.containsKey(contextName)) contextDatas.put(contextName, new ArrayList<>());
-            contextDatas.get(contextName).add(data);
-        });
+        if (getContextNames(data).size() > 1) {
+            if (!contextDatas.containsKey("shared")) contextDatas.put("shared", new ArrayList<>());
+            contextDatas.get("shared").add(data);
+        } else {
+            getContextNames(data).stream().forEach(contextName -> {
+                if (!contextDatas.containsKey(contextName)) contextDatas.put(contextName, new ArrayList<>());
+                contextDatas.get(contextName).add(data);
+            });
+        }
     }
 
-
-    private JavaClassSource generateIndicesLookup(String contextName, List<ComponentData> data) {
+    private JavaClassSource generateIndicesLookup(String contextName, List<ComponentData> dataList) {
         String pkgDestiny = targetPackageConfig.targetPackage();
 
         JavaClassSource javaClass = Roaster.parse(JavaClassSource.class, String.format("public class %1$s {}",
                 WordUtils.capitalize(contextName) + DEFAULT_COMPONENT_LOOKUP_TAG));
+        if (dataList.size() > 0 && contextName == "shared") {
+            pkgDestiny += "." + "shared";
 
-        if (data.size() > 0 && !pkgDestiny.endsWith(data.get(0).getSubDir())) {
-            pkgDestiny += "." + data.get(0).getSubDir();
+        } else if (dataList.size() > 0 && !pkgDestiny.endsWith(dataList.get(0).getSubDir())) {
+            pkgDestiny += "." + dataList.get(0).getSubDir();
 
         }
         javaClass.setPackage(pkgDestiny);
 
-        addIndices(data, javaClass);
-        addComponentNames(data, javaClass);
-        addComponentTypes(data, javaClass);
+        addIndices(dataList, javaClass);
+        addComponentNames(dataList, javaClass);
+        addComponentTypes(dataList, javaClass);
         System.out.println(javaClass);
+
         return javaClass;
     }
 
 
-    public JavaClassSource addIndices(List<ComponentData> datas, JavaClassSource javaClass) {
-        Integer index = 0;
-        for (ComponentData data : datas) {
-            if (data != null) {
-                javaClass.addField()
-                        .setName(getTypeName(data))
-                        .setType("int")
-                        .setLiteralInitializer(String.valueOf(index++))
-                        .setPublic()
-                        .setStatic(true)
-                        .setFinal(true);
-            }
-        }
-        javaClass.addField()
+    public JavaClassSource addIndices(List<ComponentData> dataList, JavaClassSource codeGenerated) {
+        final Integer[] index = {0};
+        dataList.stream()
+                //.sorted((a, b) -> a.getSource().getName().compareTo(b.getSource().getName()))
+                .map(d -> {
+                    d.put("index", index[0]++);
+                    return d;
+                })
+                .forEach(data -> {
+                    if (data != null) {
+                        codeGenerated.addField()
+                                .setName(getTypeName(data))
+                                .setType("int")
+                                .setLiteralInitializer(data.get("index").toString())
+                                .setPublic()
+                                .setStatic(true)
+                                .setFinal(true);
+                    }
+                });
+        codeGenerated.addField()
                 .setName("totalComponents")
                 .setType("int")
-                .setLiteralInitializer(Integer.toString(datas.size()))
+                .setLiteralInitializer(Integer.toString(dataList.size()))
                 .setPublic()
                 .setStatic(true)
                 .setFinal(true);
 
-        return javaClass;
+        return codeGenerated;
 
     }
 
-    public void addComponentNames(List<ComponentData> dataList, JavaClassSource javaClass) {
+    public void addComponentNames(List<ComponentData> dataList, JavaClassSource codeGenerated) {
         String format = " \"%1$s\",\n";
         String code = " return new String[] {";
 
-        ArrayList<ComponentData> totalData = new ArrayList<>(Collections.nCopies(dataList.size(), null));
-        for (ComponentData info : dataList) {
-            totalData.add(info.index, info);
-        }
-        totalData.subList(dataList.get(0).totalComponents, totalData.size()).clear();
-        for (int i = 0; i < totalData.size(); i++) {
-            ComponentData info = totalData.get(i);
-            if (info != null && info.index == i) {
-                code += String.format(format, getTypeName(dataList));
-            } else {
-                code += " null,\n";
-            }
+        for (ComponentData data : dataList) {
+            code += String.format(format, getTypeName(data));
 
         }
-
 
         StringBuilder codeFinal = new StringBuilder(code);
         if (code.endsWith(",\n")) {
             codeFinal.replace(code.lastIndexOf(",\n"), code.lastIndexOf(",") + 1, " }; ");
         }
-        javaClass.addMethod().setName("componentNames")
+        codeGenerated.addMethod().setName("componentNames")
                 .setReturnType("String[]")
                 .setPublic()
                 .setStatic(true)
@@ -163,30 +168,22 @@ public class ComponentLookupGenerator implements ICodeGenerator<JavaClassSource>
 
     }
 
-    public void addComponentTypes(List<ComponentData> data, JavaClassSource javaClass) {
+    public void addComponentTypes(List<ComponentData> dataList, JavaClassSource codGenerated) {
         String format = " %1$s%2$s,\n";
         String code = "return new Class[] {";
-        ArrayList<ComponentData> totalInfos = new ArrayList<>(Collections.nCopies(data.get(0).totalComponents, null));
-        for (ComponentData info : data) {
-            totalInfos.add(info.index, info);
-        }
-        totalInfos.subList(data.get(0).totalComponents, totalInfos.size()).clear();
-        for (int i = 0; i < totalInfos.size(); i++) {
-            ComponentData info = totalInfos.get(i);
-            if (info != null && info.index == i) {
-                code += String.format(format, getTypeName(data), ".class");
-                javaClass.addImport(info.fullTypeName);
-            } else {
-                code += String.format(format, null, "");
-            }
+
+        for (ComponentData data : dataList) {
+            code += String.format(format, getTypeName(data), ".class");
+            codGenerated.addImport(getFullTypeName(data));
 
         }
+
         StringBuilder codeFinal = new StringBuilder(code);
         if (code.endsWith(",\n")) {
             codeFinal.replace(code.lastIndexOf(",\n"), code.lastIndexOf(",") + 1, " }; ");
         }
 
-        javaClass.addMethod()
+        codGenerated.addMethod()
                 .setName("componentTypes")
                 .setReturnType("Class[]")
                 .setPublic()
@@ -195,23 +192,23 @@ public class ComponentLookupGenerator implements ICodeGenerator<JavaClassSource>
 
     }
 
-    public void addComponentFactories(ComponentData[] data, JavaClassSource javaClass) {
-        String format = " %1$s.class,\n";
-        String code = "return new FactoryComponent[] {";
-        for (int i = 0; i < data.length; i++) {
-            ComponentData info = data[i];
-            JavaInterfaceSource interfaceSource = Roaster.parse(JavaInterfaceSource.class, String.format("public interface Factory%1$s extends FactoryComponent {}",
-                    getTypeName(data)));
-            interfaceSource.addMethod()
-                    .setName(String.format("create%1$s", getTypeName(data)))
-                    .setReturnType(getTypeName(data))
-                    .setPublic();
-
-            javaClass.addNestedType(interfaceSource.toString());
-
-
-        }
-
-
-    }
+//    public void addComponentFactories(ComponentData[] data, JavaClassSource codeGenerated) {
+//        String format = " %1$s.class,\n";
+//        String code = "return new FactoryComponent[] {";
+//        for (int i = 0; i < data.length; i++) {
+//            ComponentData info = data[i];
+//            JavaInterfaceSource interfaceSource = Roaster.parse(JavaInterfaceSource.class, String.format("public interface Factory%1$s extends FactoryComponent {}",
+//                    getTypeName(data)));
+//            interfaceSource.addMethod()
+//                    .setName(String.format("create%1$s", getTypeName(data)))
+//                    .setReturnType(getTypeName(data))
+//                    .setPublic();
+//
+//            codeGenerated.addNestedType(interfaceSource.toString());
+//
+//
+//        }
+//
+//
+//    }
 }
---
+++ codegenerator/plugins/src/test/groovy/ilargia/entitas/codeGeneration/plugins/generators/ComponentLookupGeneratorSpec.groovy
@@ -0,0 +1,86 @@
+package ilargia.entitas.codeGeneration.plugins.generators
+
+import ilargia.entitas.codeGeneration.data.CodeGenFile
+import ilargia.entitas.codeGeneration.interfaces.IAppDomain
+import ilargia.entitas.codeGeneration.plugins.dataProviders.components.ComponentDataProvider
+import ilargia.entitas.fixtures.TestProject
+import org.gradle.api.Project
+import org.gradle.api.plugins.JavaPlugin
+import org.gradle.testfixtures.ProjectBuilder
+import org.jboss.forge.roaster.model.source.JavaClassSource
+import spock.lang.Narrative
+import spock.lang.Shared
+import spock.lang.Specification
+import spock.lang.Title
+
+@Narrative("""
+Como usuario de la aplicacion
+Quiero generar una clase con los indices de los compoenentes
+Para poder referenciarlos en la  el desarrollo en la aplicacion.
+""")
+@Title(""" """)
+//@groovy.transform.TypeChecked
+class ComponentLookupGeneratorSpec extends Specification {
+
+    @Shared
+    ComponentLookupGenerator lookupGenerator
+    @Shared
+    Project project
+    @Shared
+    ComponentDataProvider componentDataProvider
+
+    def setupSpec() {
+        lookupGenerator =  new ComponentLookupGenerator()
+        componentDataProvider = new ComponentDataProvider()
+        project = ProjectBuilder.builder().withProjectDir(new File("./")).withGradleUserHomeDir(new File("./build")).build()
+        JavaPlugin plugin = project.getPlugins().apply(JavaPlugin.class)
+        IAppDomain appProject = new TestProject(project)
+        componentDataProvider.setAppDomain(new TestProject(project))
+
+    }
+
+
+    void 'Consultamos al generador ComponentEntityGenerator por la configuracion por defecto'() {
+        given:
+        Properties prop = new Properties()
+        prop.setProperty("CodeGeneration.CodeGenerator.SearchPkg","ilargia.entitas.fixtures.components")
+
+        when:
+        componentDataProvider.configure(prop)
+        lookupGenerator.configure(prop)
+
+        then:
+        lookupGenerator.gePriority() == 0
+        lookupGenerator.getName() == "Components Lookup"
+        lookupGenerator.isEnableByDefault() == true
+        lookupGenerator.runInDryMode() == true
+
+    }
+
+
+    void 'Consultamos al generador ComponentEntityGenerator por las propiedades que se extraen'() {
+        given:
+        Properties prop = new Properties()
+        prop.setProperty("CodeGeneration.CodeGenerator.SearchPkg","ilargia.entitas.fixtures.components")
+        componentDataProvider.configure(prop)
+        lookupGenerator.configure(prop)
+        lookupGenerator.getDefaultProperties()
+
+        when:
+        List<CodeGenFile<JavaClassSource>> genFiles = lookupGenerator.generate(componentDataProvider.getData())
+
+        then:
+        genFiles.size() == 3
+        genFiles.get(0).fileName == "SharedComponentsLookup"
+        genFiles.get(0).fileContent.getPackage() == "entitas.generated.shared"
+        genFiles.get(1).fileName == "GameComponentsLookup"
+        genFiles.get(1).subDir == "game"
+        genFiles.get(1).fileContent.getPackage() == "entitas.generated.game"
+        genFiles.get(2).fileName == "TestComponentsLookup"
+        genFiles.get(2).subDir == "test"
+        genFiles.get(2).fileContent.getPackage() == "entitas.generated.test"
+
+
+    }
+
+}
